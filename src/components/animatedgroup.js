cd1803808f5b9ec6098ca3c36d014c7edb65a32c
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-03-27 10:23:04 +0100

    added home illustration animation

---
+++ src/components/animatedgroup.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {animated, useSpring} from 'react-spring'
+
+import './animatedgroup.css'
+
+function AnimatedGroup({children, delay}){
+  
+  const [opacity, set, stop] = useSpring(() => ({
+    from:{
+      opacity:0,
+      transform:`scale(1.1)`
+    },
+    opacity:1,
+    transform:'scale(1)',
+    delay:0 || delay * 1.5
+  }))
+
+  return(
+    <animated.g 
+      className='animated-group' 
+      style={opacity}
+      onPointerOver={() => set({transform:'scale(1.01)'})}
+      onPointerOut={() => set({transform:'scale(1)'})}
+    >
+      {children}
+    </animated.g>
+  )
+}
+
+export default AnimatedGroup
